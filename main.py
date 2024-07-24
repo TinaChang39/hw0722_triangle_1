1027fd07faf3afa3a2f2216d8a47c4ae474b4c88
Tina <changtina39@gmail.com>
2024-07-24 16:03:21 +0800

    hw1&hw2
    作業一: 畫出等腰三角形
    作業二: 九九乘法表

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 print("Hello World")
+
+# 作業一: 畫出等腰三角形
+n = 0
+n = int(input('請輸入三角形層數:'))
+for i in range(1,n+1):               # 行數
+    for j in range(1,n-i+1):         # 空幾格
+        print(' ',end = '')
+    for k in range(1,2*i):           # 印幾個*
+        print('*',end = '')
+    print('\n',end = '')
+
+
+# 作業二: 九九乘法表
+for i in range(1,10):
+    for j in range(1,10):
+        print('{0} x {1} = {2}\t'.format(j,i,i*j),end='')
+    print('\n',end='')
+
+# 作業三: 找出質數
+
+
