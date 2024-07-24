bb964cd9fc42fc152f5db32daff4fda12cbef1f0
Tina <changtina39@gmail.com>
2024-07-24 20:02:07 +0800

    pp

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+print("Hello World")
+
 # 作業一: 畫出等腰三角形
 n = 0
 n = int(input('請輸入三角形層數:'))
@@ -8,12 +10,14 @@ for i in range(1,n+1):               # 行數
         print('*',end = '')
     print('\n',end = '')
 
+
 # 作業二: 九九乘法表
 for i in range(1,10):
     for j in range(1,10):
         print('{0} x {1} = {2}\t'.format(j,i,i*j),end='')
     print('\n',end='')
 
+<<<<<<< HEAD
 # 作業三: 找出質數 (輸入數值 -> 計算1~數值中有幾個質數 -> 印出所有質數)
 num = 0
 count = 0
@@ -34,4 +38,8 @@ for i in range(2,num+1):           # 小於數值的所有數字
         
 print('\n',end='') 
 print('質數總個數:',count,end = '')
+=======
+# 作業三: 找出質數
+
+>>>>>>> parent of 2d9533c (try_hw3)
 
